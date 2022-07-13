796a516064259239b28a61fb404b3f5f7b3a3c71
joshroche1 <joshua.a.roche@gmail.com>
2022-07-13 17:23:34 +0200

    .

--- src/main/java/net/jar/ServerManagementConsole/Controller/DBUtil.java
+++ src/main/java/net/jar/ServerManagementConsole/Controller/DBUtil.java
@@ -43,7 +43,7 @@ public class DBUtil {
   public ResultSet getServers() {
     try {
       conn = this.connect();
-      PreparedStatement pstmt = conn.prepareStatement("SELECT active, hostname, provider, type, os, distro, cpuCores, memory, diskSize, publicIpAddress, fqdn, notes FROM server", ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      PreparedStatement pstmt = conn.prepareStatement("SELECT active, hostname, provider, type, os, distro, cpuCores, memory, diskSize, publicIpAddress, fqdn, notes FROM servers", ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
       rs = pstmt.executeQuery();
     } catch (Exception ex) {
       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
